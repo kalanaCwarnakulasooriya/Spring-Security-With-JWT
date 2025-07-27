10d352e9edf7f7a8acbd5577499702344d0af98d
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-07-27 22:57:28 +0530

    RegisterDTO

--- Back_End/src/main/java/lk/ijse/back_end/dto/RegisterDTO.java
+++ Back_End/src/main/java/lk/ijse/back_end/dto/RegisterDTO.java
@@ -1,4 +1,10 @@
 package lk.ijse.back_end.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterDTO {
+    private String password;
+    private String username;
+    private String role; //USER or ADMIN
 }
