d103bc84128d03733b5095be63e5b8e10046da72
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-09 10:28:56 +0530

    Update

--- Back_End/src/main/java/lk/ijse/back_end/controller/HelloController.java
+++ Back_End/src/main/java/lk/ijse/back_end/controller/HelloController.java
@@ -1,12 +1,14 @@
 package lk.ijse.back_end.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin("*")
 public class HelloController {
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER')")
